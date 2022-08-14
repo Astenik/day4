37f4f67efecca032a45379ae5ab846d06a6f4e9c
Astenik <106985461+Astenik@users.noreply.github.com>
2022-08-14 19:03:20 +0400

    Update num_indexes.py

--- num_indexes.py
+++ num_indexes.py
@@ -1,30 +1,28 @@
 lst = [1, 2, 3, 4, 4, 4]
 number = int(input("insert number: "))
 def num_index(lst, num):
-        '''this function findes the firs and last indexes 
+         '''this function findes the firs and last indexes 
         of num in lst list, if num is not in list function returns 
         [-1, -1].'''
         if (lst[-1] < num) or (lst[0] > num):
                 return [-1, -1]
         res = []
         ind = 0
+        rev_lst = lst[::-1]
         while lst[ind] <= num:
                if lst[ind] == num:
                      res.append(ind)
                      break 
                ind += 1
-        if len(res) == 0:
-             res = [-1, -1]
-        elif res[0] == (len(lst) - 1):
-                res.append(-1)
         else:
-              for ind1 in range(res[0], len(lst)):
-                       if lst[ind1] == num and (len(res) == 1):
-                                 res.append(ind1)
-                       elif lst[ind1] == num:
-                              res.pop()
-                              res.append(ind1)
-                       else:
-                           break
+             res = [-1, -1]
+        if res[0] == (len(lst) - 1):
+                res.append(res[0])
+        else:
+              for i in range(len(lst) - res[0]):
+                  if rev_lst[i] == num:
+                      res.append(len(lst) - i - 1)
+                      break 
         return res 
-print(num_index(lst, number))
+print(f"firs and last indexes of num in lst are: {num_index(lst, number)}")
+
